74aa023e2e4dc5a0ec798528caf7d10e264a6541
erendeveloper-admin <erendeveloper@gmail.com>
2024-10-16 16:25:29 +0300

    update sound controller
    update sound controller

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -14,44 +14,101 @@ public class SoundController : MonoBehaviour
     [SerializeField] private AudioSource moneySound;
     [SerializeField] private AudioSource musicSound;
     
+    private bool isVolumeOn=false;
+    private bool isVibrationOn = false;
 
     private void Awake()
     {
         instance = this;
+        CheckVolumeOnOff();
+        CheckVibrationOnOff();
+        AudioListener.volume = 1f;
+        
     }
 
     public static void SetVolume(float volume)
     {
         AudioListener.volume= volume;
     }
-
-    public void PlayClickSound()
+    public static void SetVibration(bool statue)
     {
+        instance.isVibrationOn= statue;
+    }
+
+    public static void PlayClickSound()
+    {
+        //playSound(instance.clickSound);
         instance.clickSound.Play();
+        vibrate();
     }
     public static void PlayTapSound()
     {
+        //playSound(instance.tapSound);
         instance.tapSound.Play();
+        vibrate();
     }
     public static void PlayCrashSound()
     {
+        //playSound(instance.crashSound);
         instance.crashSound.Play();
+        vibrate();
     }
     public static void PlayWinSound()
     {
+        //playSound(instance.winSound);
         instance.winSound.Play();
     }
     public static void PlayLoseSound()
     {
+        //playSound(instance.loseSound);
         instance.loseSound.Play();
     }
     public static void PlayMoneySound()
     {
+        //playSound(instance.moneySound);
         instance.moneySound.Play();
-
     }
-    public static void PlayMusicSound()
+    public static void PlayMusicSound()  //play on awake
     {
-        instance.musicSound.Play();
+        //instance.musicSound.Play();  
+    }
+
+    //private void playSound(AudioSource audioSource)
+    //{
+    //    if (instance.isVolumeOn)
+    //    {
+    //        audioSource.Play();
+    //    }
+    //}
+    public static void CheckVolumeOnOff()
+    {
+        if (SaveLoadGameInfo.LoadAuidoOnOff())
+        {
+            instance.isVolumeOn = true;
+            SetVolume(1f);
+        }
+        else
+        {
+            instance.isVolumeOn = false;
+            SetVolume(0f);
+        }
+    }
+    public static void CheckVibrationOnOff()
+    {
+        if (SaveLoadGameInfo.LoadVibrationOnOff())
+        {
+            instance.isVibrationOn = true;
+        }
+        else
+        {
+            instance.isVibrationOn = false;
+        }
+    }
+    private static void vibrate()
+    {
+        if (instance.isVibrationOn)
+        {
+            Handheld.Vibrate();
+        }
     }
 }
